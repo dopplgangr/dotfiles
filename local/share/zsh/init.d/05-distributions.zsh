f281d2b0dfe1d9cdb1b3269d25cd1c19db9c6f9d
dopplgangr <dopplgangr@gmail.com>
2019-02-12 13:47:42 -0500

    added some distribution inits

--- local/share/zsh/init.d/05-distributions.zsh
+++ local/share/zsh/init.d/05-distributions.zsh
@@ -1,3 +1,15 @@
+# check for intel opt package
+if [[ -d /opt/intel/vtune_amplifier ]]; then
+	# Add quiet flag to shut up that annoying message
+	source /opt/intel/vtune_amplifier/amplxe-vars.sh "quiet"
+fi
+
 if [[ $(lsb_release -is) == "CentOS" ]]; then
+  #
   # CentOS specific features
+  #
+
+  # check for alternative git
+  [[ -f /opt/rh/rh-git29/enable ]] && source /opt/rh/rh-git29/enable
+  [[ -f /opt/rh/rh-python36/enable ]] && source /opt/rh/rh-python36/enable
 fi
