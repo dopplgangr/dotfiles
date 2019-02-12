223a1c19fd5dbd6dc4b1192a30e182d9a219857b
dopplgangr <dopplgangr@gmail.com>
2019-02-12 12:30:14 -0500

    - updated _git_status activation logic - added autoloading of functions to 04-functions.zsh - removed sample.yml

--- local/share/zsh/init.d/04-functions.zsh
+++ local/share/zsh/init.d/04-functions.zsh
@@ -1,1 +1,7 @@
-fpath=( ~/.local/share/zsh/functions.d "${fpath[@]}" )
+DOTFILES_ZSH_FUNCTIONS=~/.local/share/zsh/functions.d
+if [[ -d ${DOTFILES_ZSH_FUNCTIONS} ]]; then
+  fpath=( "${DOTFILES_ZSH_FUNCTIONS}" "${fpath[@]}" )
+  for func in ${DOTFILES_ZSH_FUNCTIONS}/*(:t); do
+    autoload -Uz "${func}"
+  done
+fi
