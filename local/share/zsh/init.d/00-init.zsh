57cb188918456abfdc53de1de64f93615b26d576
dopplgangr <dopplgangr@gmail.com>
2019-02-14 10:15:18 -0500

    fixed theme response time, updated nvim configs

--- local/share/zsh/init.d/00-init.zsh
+++ local/share/zsh/init.d/00-init.zsh
@@ -8,7 +8,27 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-history-substring-search", defer:2
 zplug "zsh-users/zsh-syntax-highlighting", defer:3
-#zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
+zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
+# module order
+SPACESHIP_PROMPT_ORDER=(
+	user
+	host
+	char
+	dir
+	git
+	docker
+)
+
+SPACESHIP_USER_PREFIX=""
+SPACESHIP_USER_SUFFIX=""
+
+#HOST
+SPACESHIP_HOST_PREFIX="@:("
+SPACESHIP_HOST_SUFFIX=") "
+
+SPACESHIP_DIR_PREFIX='' # disable directory prefix, cause it's not the first section
+SPACESHIP_DIR_TRUNC='1' # show only last directory
+
 
 # Install plugins that haven't been installed
 if ! zplug check --verbose; then
