15220763efc8a254f7f31f578ad6f08b4b13780b
dopplgangr <dopplgangr@gmail.com>
2019-02-27 18:19:17 -0500

    added dircolors to 05-terminfo, it's also under common, tbd what to do with it

--- local/share/zsh/init.d/05-terminfo.zsh
+++ local/share/zsh/init.d/05-terminfo.zsh
@@ -2,3 +2,7 @@ if ! infocmp tmux-256color &> /dev/null; then
   log_warning "regenerating tmux-256color"
   tic -x ~/.local/share/terminfo/tmux
 fi
+
+if [ -f ~/.dircolors ]; then
+  eval "$(command dircolors ~/.dircolors)"
+fi
