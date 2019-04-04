cc2e58d4db39a92816681901356dea36ee3560ae
Christian Todie <ctodie@hanweckassoc.com>
2019-04-04 15:59:56 -0400

    fixed dircolors actually

--- zsh/init.d/05-terminfo.zsh
+++ zsh/init.d/05-terminfo.zsh
@@ -3,6 +3,7 @@ if ! infocmp tmux-256color &> /dev/null; then
   tic -x ~/.local/share/terminfo/tmux
 fi
 
-if [ -f ~/.config/dircolors ]; then
-  eval "$(command dircolors ~/.dircolors)"
+DOTFILES_DIRCOLORS=${HOME}/.config/dircolors
+if [ -f ${DOTFILES_DIRCOLORS} ]; then
+  eval "$(command dircolors ${DOTFILES_DIRCOLORS})"
 fi
