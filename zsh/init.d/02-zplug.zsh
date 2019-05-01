dd7172cb972b9d336f9cef3d27c69c7db685540f
dopplgangr <dopplgangr@gmail.com>
2019-05-01 14:25:11 -0400

    added uninstall script

--- zsh/init.d/02-zplug.zsh
+++ zsh/init.d/02-zplug.zsh
@@ -12,6 +12,8 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:3
 zplug "DFurnes/purer", use:pure.zsh, from:github, as:theme
 PURE_PROMPT_SYMBOL="»"
 PURE_PROMPT_VICMD_SYMBOL="«"
+PURE_GIT_DOWN_ARROW="↑"
+PURE_GIT_UP_ARROW="↓"
 # ---
 zplug "tarruda/zsh-autosuggestions", use:"zsh-autosuggestions.zsh"
 zplug "mafredri/zsh-async", from:github
